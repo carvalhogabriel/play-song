ba5165cc46ba1b8d80a554d9a16e52839df5ecff
Gabriel Guerrero <gabriel.guerrero@dextra-sw.com>
2020-12-01 20:18:58 -0300

    create Play Song project

---
+++ Play Song/Play Song/App/Play_SongApp.swift
@@ -0,0 +1,17 @@
+//
+//  Play_SongApp.swift
+//  Play Song
+//
+//  Created by Gabriel Carvalho Guerrero on 01/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Play_SongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
